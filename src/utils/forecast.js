00c3ac1c37560cb9e10a2f7749a6236642a2f016
SOHTA0318 <nicosohta0318@gmail.com>
2021-11-03 19:38:30 -0700

    fix

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,17 +1,20 @@
-const request = require('request')
-
-const forecast = (latitude, longitude, callback) => {
-    const url = 'https://api.darksky.net/forecast/9d1465c6f3bb7a6c71944bdd8548d026/' + latitude + ',' + longitude
-
-    request({ url, json: true }, (error, { body }) => {
-        if (error) {
-            callback('Unable to connect to weather service!', undefined)
-        } else if (body.error) {
-            callback('Unable to find location', undefined)
-        } else {
-            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degress out. There is a ' + body.currently.precipProbability + '% chance of rain.')
-        }
-    })
-}
-
-module.exports = forecast
+const request = require("request");
+const forecast = (lon, lat, callback) => {
+  const url = `http://api.weatherstack.com/current?access_key=81794ee4b0f74e5cd056d9a3b21ce10f&query=${encodeURIComponent(
+    lat
+  )},${encodeURIComponent(lon)}&units=f`;
+  request({ url, json: true }, (error, { body }) => {
+    if (error) {
+      callback("Unable to connect to weather service", undefined);
+    } else if (body.error) {
+      callback("Unable to find location", undefined);
+      // callback(body.error.info);
+    } else {
+      callback(
+        undefined,
+        `${body.current.weather_descriptions[0]} It is currently ${body.current.temperature} degrees out. It feels like ${body.current.feelslike} degrees out`
+      );
+    }
+  });
+};
+module.exports = forecast;
